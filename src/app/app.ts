b4caa607d07c656a8e7c4b302f4ed1fdde1268b7
vilmar2025 <vilmarpereiradesouzafilho@gmail.com>
2025-10-06 00:24:05 -0300

    add rotas

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,16 @@ import { Component} from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar";
 import{CommonModule} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { PrimaryButton } from "./_components/primary-button/primary-button";
-import { SecondaryButton } from "./secondary-button/secondary-button";
-import { Itemcertificado } from "./_components/itemcertificado/itemcertificado";
+
+
+
 import { BaseUi } from "./_components/base-ui/base-ui";
-import { Certificados } from "./certificados/certificados";
-import { Certificadoform } from "./pages/certificadoform/certificadoform";
-import { Certificado } from "./pages/certificado/certificado";
+
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButton, SecondaryButton, Itemcertificado, BaseUi, Certificados, Certificadoform, Certificado],
+  imports: [RouterOutlet, NavbarComponent, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
